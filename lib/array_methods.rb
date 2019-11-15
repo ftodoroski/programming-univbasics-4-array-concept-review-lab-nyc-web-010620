e998f629fcacfd9f385b0a73fbf7c28448898733
Filip Todoroski <ftodoroski@ymail.com>
2019-11-15 14:32:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -24,5 +24,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
+  smallest_value = array[0]
+  length = array.length
 
+  length.times do |i|
+    value = array[i]
+
+    smallest_value = value if value < smallest_value
+  end
 end
